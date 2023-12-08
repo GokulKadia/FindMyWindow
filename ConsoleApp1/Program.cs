bdb8fd7e52c73a29b6275ee7a12aa367f337e47f
gokul_kadia <gokul.kadia@dellteam.com>
2023-12-08 11:28:11 +0530

    Find My Window

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        static void Main(string[] args)
+        {
+
+            while(true)
+            {
+                // Get the foreground window handle
+                IntPtr foregroundWindowHandle = GetForegroundWindow();
+                // Get the process ID associated with the foreground window
+                uint foregroundProcessId;
+                GetWindowThreadProcessId(foregroundWindowHandle, out foregroundProcessId);
+                // Get the process name using the process ID
+                string foregroundProcessName = Process.GetProcessById((int)foregroundProcessId).ProcessName;
+                Console.WriteLine("Foreground Process Name: " + foregroundProcessName);
+
+
+
+                // Find and kill the Explorer when Vmware-View exe found
+                //Process.Start(@"C:\Windows\System32\taskkill.exe", @"/F /IM explorer.exe");
+                ////if (foregroundProcessName == "vmware-view")
+                ////{
+                ////    Process.GetProcesses().Where(x => x.ProcessName == "explorer").ToList().ForEach(x => x.Kill());                    
+                ////}
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _proc = HookCallback;
+            _hookID = SetHook(_proc);
+            //UnhookWindowsHookEx(_hookID);
+
+        }
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int VK_LWIN = 0x5B; // Left Windows key
+        private const int VK_X = 0x58; // X key
+
+        private static LowLevelKeyboardProc _proc;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (vkCode == VK_X && IsKeyDown(VK_LWIN))
+                {
+                    MessageBox.Show("Windows Key + X pressed");
+                    // Handle your custom logic here
+                }
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static bool IsKeyDown(int keyCode)
+        {
+            return (GetAsyncKeyState(keyCode) & 0x8000) != 0;
+        }
+
+        //private void Window_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if(Keyboard.IsKeyDown((Keyboard.Modifiers & ModifierKeys.Windows) == ModifierKeys.Windows) && Keyboard.IsKeyDown(Key.C))
+        //    {
+        //        System.Windows.MessageBox.Show("Gokul");
+        //    }
+        //}
+    }
+}
